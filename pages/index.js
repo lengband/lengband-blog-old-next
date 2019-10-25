c938f279948f64b9231a2d917c25e563b339890c
wangpeng <email@qq.com>
2019-10-25 23:27:19 +0800

    add axios

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import Head from 'next/head'
 import { Row, Col, List, Icon } from 'antd'
 import Header from '../components/Header'
@@ -7,39 +8,34 @@ import Advert from '../components/Advert'
 import Footer from '../components/Footer'
 import '../static/style/pages/index.css'
 
-const Home = () => {
-  const [mylist, setMylist] = useState(
-    [
-      { title: '聊聊浏览器的渲染机制', context: '呈现引擎将开始解析 HTML 文档，并将各标记逐个转化成“内容树”上的 DOM 节点。同时也会解析外部 CSS 文件以及样式元素中的样式数据。HTML 中这些带有视觉指令的样式信息将用于创建另一个树结构：呈现树。呈现树包含多个带有视觉属性（如颜色和尺寸）的矩形。这些矩形的排列顺序就是它们将在屏幕上显示的顺序。 呈现树构建完毕之后，进入“布局”处理阶段，也就是为每个节点分配一个应出现在屏幕上的确切坐标。下一个阶段是绘制 - 呈现引擎会遍历呈现树，由用户界面后端层将每个节点绘制出来。' },
-      { title: 'CSS中的BFC，外边距折叠，清除浮动', context: '呈现引擎将开始解析 HTML 文档，并将各标记逐个转化成“内容树”上的 DOM 节点。同时也会解析外部 CSS 文件以及样式元素中的样式数据。HTML 中这些带有视觉指令的样式信息将用于创建另一个树结构：呈现树。呈现树包含多个带有视觉属性（如颜色和尺寸）的矩形。这些矩形的排列顺序就是它们将在屏幕上显示的顺序。呈现树构建完毕之后，进入“布局”处理阶段，也就是为每个节点分配一个应出现在屏幕上的确切坐标。下一个阶段是绘制 - 呈现引擎会遍历呈现树，由用户界面后端层将每个节点绘制出来。' },
-      { title: 'CSS中line-height与vertical-align', context: '呈现引擎将开始解析 HTML 文档，并将各标记逐个转化成“内容树”上的 DOM 节点。同时也会解析外部 CSS 文件以及样式元素中的样式数据。HTML 中这些带有视觉指令的样式信息将用于创建另一个树结构：呈现树。呈现树包含多个带有视觉属性（如颜色和尺寸）的矩形。这些矩形的排列顺序就是它们将在屏幕上显示的顺序。呈现树构建完毕之后，进入“布局”处理阶段，也就是为每个节点分配一个应出现在屏幕上的确切坐标。下一个阶段是绘制 - 呈现引擎会遍历呈现树，由用户界面后端层将每个节点绘制出来。' },
-      { title: 'React Hooks 简介', context: '呈现引擎将开始解析 HTML 文档，并将各标记逐个转化成“内容树”上的 DOM 节点。同时也会解析外部 CSS 文件以及样式元素中的样式数据。HTML 中这些带有视觉指令的样式信息将用于创建另一个树结构：呈现树。呈现树包含多个带有视觉属性（如颜色和尺寸）的矩形。这些矩形的排列顺序就是它们将在屏幕上显示的顺序。呈现树构建完毕之后，进入“布局”处理阶段，也就是为每个节点分配一个应出现在屏幕上的确切坐标。下一个阶段是绘制 - 呈现引擎会遍历呈现树，由用户界面后端层将每个节点绘制出来。' },
-    ]
-  )
+const Home = (list) => {
+  const [mylist, setMylist] = useState(list.data);
   return (
     <>
       <Head>
-        <title>冷板凳博客</title>
+        <title>Home</title>
       </Head>
       <Header />
       <Row className="comm-main" type="flex" justify="center">
         <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
-          <List
-            header={<div>最新日志</div>}
-            itemLayout="vertical"
-            dataSource={mylist}
-            renderItem={item => (
-              <List.Item>
-                <div className="list-title">{item.title}</div>
-                <div className="list-icon">
-                  <span><Icon type="calendar" /> 2019-06-28</span>
-                  <span><Icon type="folder" /> 视频教程</span>
-                  <span><Icon type="fire" /> 5498人</span>
-                </div>
-                <div className="list-context">{item.context}</div>
-              </List.Item>
-            )}
-          />
+          <div>
+            <List
+              header={<div>最新日志</div>}
+              itemLayout="vertical"
+              dataSource={mylist}
+              renderItem={item => (
+                <List.Item>
+                  <div className="list-title">{item.title}</div>
+                  <div className="list-icon">
+                    <span><Icon type="calendar" /> {item.addTime}</span>
+                    <span><Icon type="folder" /> {item.typeName}</span>
+                    <span><Icon type="fire" /> {item.view_count}人</span>
+                  </div>
+                  <div className="list-context">{item.introduce}</div>
+                </List.Item>
+              )}
+            />
+          </div>
         </Col>
         <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
@@ -49,6 +45,18 @@ const Home = () => {
       <Footer />
     </>
   )
+}
+
+Home.getInitialProps = async () => {
+  const promise = new Promise((resolve) => {
+    axios('http://127.0.0.1:7001/default/getArticleList').then(
+      (res) => {
+        console.log('远程获取数据结果:', res.data.data)
+        resolve(res.data)
+      }
+    )
+  })
+  return await promise
 }
 
 export default Home
